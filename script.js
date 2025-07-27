9d2fd7640a53372bd49b72e1b267efacdfa2573b
dottcroce <dottorcroce74@gmail.com>
2025-07-27 15:53:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,30 @@
 function openLocation(localita) {
   window.location.href = `localita/${localita}.html`;
 }
+
+
+// Applica animazione marker se Leaflet è usato
+if (typeof L !== 'undefined') {
+    const originalIcon = L.Icon.Default.prototype.createIcon;
+    L.Icon.Default.prototype.createIcon = function (oldIcon) {
+        var icon = originalIcon.call(this, oldIcon);
+        icon.classList.add("animated");
+        return icon;
+    };
+}
+
+// Gestione stelline
+document.addEventListener('DOMContentLoaded', function () {
+  const stars = document.querySelectorAll('.star');
+  stars.forEach((star, idx) => {
+    star.addEventListener('click', () => {
+      stars.forEach((s, i) => {
+        if (i <= idx) {
+          s.classList.add('selected');
+        } else {
+          s.classList.remove('selected');
+        }
+      });
+    });
+  });
+});
